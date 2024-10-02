a18c10fc2f5c24d76a5fb80d72ba32a15267fd0e
salmanals <salman.al.salem727@gmail.com>
2024-10-02 21:08:12 +0300

    Dart Lists

--- main.dart
+++ main.dart
@@ -1,7 +1,74 @@
+import 'dart:html_common';
+import 'dart:math';
+
 void main(){
-  // Write your code here
+  var integers = [5, 3, 15, 4, 1];  
+  int addition = 0; 
+  int largest= 0; 
+
+  integers.forEach((sum){
+    addition= addition+sum ;
+    print(addition);
+    });
+
+  integers.forEach((num){
+    
+    largest= max(largest, num);
+    
+
+
+  });
+  print(largest);
+
+ 
+  List <int> listofAge = [10,15,21,12,31,18]; 
+  filterVisitors (listofAge,18);
+  print(filterVisitors);
+   
+
   
+  List <int> num = [9,7,19,14,33,18]; 
+
+}  
+
+void filterVisitors (listofAge,minimumAge){
+
   
-  
-  
+
+
+
+  List <int> filterVisitors(List<int> listofAge, int minimumAge) {
+  return listofAge.where((listofAge) => listofAge > minimumAge).toList();
 }
+
+
+    
+
+
+
+
+
+
+
+
+  }
+
+  void findOdds(num,oddnum){
+
+(List<int> num, int oddnum)
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
